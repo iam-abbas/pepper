47435b130640de3880112776329addd3105a8272
iam-abbas <iamabbas7@gmail.com>
2020-05-18 04:08:22 -0700

    social links updated

--- packages/pepper-portfolio/src/components/Socials.js
+++ packages/pepper-portfolio/src/components/Socials.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Socials({ link, name }) {
     return (
-      <a className="social-link" href={link}><i className={"fab fa-"+name}></i></a>
+      <a className="social-link" target="_blank" href={link}><i className={"fab fa-"+name}></i></a>
     )
   }
